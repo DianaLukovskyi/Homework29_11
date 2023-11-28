9d2d540dc78e2a46a8278e62a452463348082313
DianaLukovskyi <151841443+DianaLukovskyi@users.noreply.github.com>
2023-11-28 23:51:04 +0300

    Homework.java

--- src/Homework.java
+++ src/Homework.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Homework {
+public class Homework {
+    public static void main(String[] args) {
+
+
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {1, 2, 3};
+        int[] arr3 = {1, 2, 3};
+        System.out.println("if arr1 equal err2" + areEqual(arr1,arr2));
+        System.out.println("if arr1 egual err3" + areEqual(arr1,arr3));
+    }
+
+    public static boolean areEqual(int[] array1, int[] array2) {
+        if (array1.length != array2.length) {
+            return false;
+
+        }
+
+        return true;
+    }
 }
--- src/Homework2.java
+++ src/Homework2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Homework2 {
+public class Homework2 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 8, 7};
+        int count = countNum(array);
+        System.out.println("quantity ever count: " + count);
+
+    }
+
+    public static int countNum(int[] arra) {
+        int count = 0;
+        for (int num : arra) {
+            if (num % 2 == 0) {
+                count++;
+
+            }
+
+
+        }
+        return count;
+    }
 }
